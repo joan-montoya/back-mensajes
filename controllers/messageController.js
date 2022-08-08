20cd6c8c8b27104524055c9c3477ebc330144f96
Karla Moriana Rodriguez Yllescas <moriAeseces@gmail.com>
2022-08-08 05:21:58 -0500

    feat: message function changed

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -23,13 +23,13 @@ module.exports.getMessages = async (req, res, next) => {
 };
 
 module.exports.addMessage = async (req, res, next) => {
-  try {
-    const { from, to, message } = req.body;
-    const data = await Messages.create({
-      message: { text: message },
-      users: [from, to],
-      sender: from,
-    });
+    try {
+      const { from, to, message } = req.body;
+      const data = await Messages.create({
+        message: { text: message },
+        users: [from, to],
+        sender: from,
+      });
 
     if (data) return res.json({ msg: "Message added successfully." });
     else return res.json({ msg: "Failed to add message to the database" });
