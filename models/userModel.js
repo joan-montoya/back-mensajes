ed0834b4523717e3161919231ec451d2f220093a
cecilia-tisca <ceciliatisca@gmail.com>
2022-08-08 03:56:52 -0500

    Feat: Information and characteristics required by the password

--- models/userModel.js
+++ models/userModel.js
@@ -12,6 +12,11 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
         max: 50,
+      },
+      password: {
+        type: String,
+        required: true,
+        min: 8,
       }
 });
 
